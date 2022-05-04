f6b0ba992188c4dda592badf4256000ca16745c7
Ryder <rydersfw@gmail.com>
2022-05-04 17:51:09 +0800

    board# get_neighbors completed

--- board.rb
+++ board.rb
@@ -48,6 +48,34 @@ class Board
             end
         end
     end
+    def reveal_pos(pos)
+        self[pos].reveal
+    end
+    def get_neighbors(pos)
+        #returns the positions of all neighbors of pos
+        #returns as array of pos e.g. [[pos1], [pos2], [pos3]]
+        neighbor_positions = []
+        row, ele = pos[0], pos[1]
+        #get the diagonal neighbors
+        neighbor_positions << [row - 1, ele - 1] if row > 0 && ele > 0
+        neighbor_positions << [row - 1, ele + 1] if row > 0 && ele < 8
+        neighbor_positions << [row + 1, ele + 1] if row < 8 && ele < 8
+        neighbor_positions << [row + 1, ele - 1] if row < 8 && ele > 0
+        neighbor_positions << [row - 1, ele] if row > 0
+        neighbor_positions << [row, ele + 1] if ele < 8
+        neighbor_positions << [row + 1, ele] if row < 8
+        neighbor_positions << [row, ele - 1] if ele > 0 
+    end
+    def gen_value(pos)
+        #call count_bombs_neighbors(get_neighbors(pos))
+        #set value of tile at pos to count of bombs
+    end
+    def count_bombs_neighbors(array_of_positions)
+        #return num of bomb tiles in array_of_pos
+    end
+    def reveal_neighbors(pos)
+        #reveals all neighboring tiles of pos
+    end
 end
 
 if __FILE__ == $PROGRAM_NAME
@@ -59,4 +87,13 @@ if __FILE__ == $PROGRAM_NAME
     board.place_bombs
     board.reveal_all_bombs
     board.render
+    board.get_neighbors([0, 0]) #1,1  0,1  1,0  
+    board.get_neighbors([4, 4]) #3,3  3,5  5,5  5,3  3,4  4,5  5,4  4,3
+    board.get_neighbors([8, 8]) #7,7  7,8  8,7
+    board.get_neighbors([8, 0]) #7,1  7,0  8,1
+    board.get_neighbors([0, 8]) #1,7  1,8  0,7
+    board.get_neighbors([8, 4]) #7,3  7,5  7,4  8,5  8,3  
+    board.get_neighbors([5, 0]) #4,1  6,1  4,0  5,1  6,0
+    board.get_neighbors([3, 8]) #2,7  4,7  2,8  4,8, 3,7
+    board.get_neighbors([0, 3]) #1,4  1,2  0,4  1,3  0,2
 end
